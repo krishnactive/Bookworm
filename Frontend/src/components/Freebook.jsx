30949d01b0d294db81147b22491eeda49123da50
Krishna kant sharma <krishnactive@gmail.com>
2025-04-19 00:02:43 +0530

    slight change

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -12,7 +12,7 @@ function Freebook() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:5173/book");
+        const res = await axios.get("http://localhost:4001/book");
 
         const data = res.data.filter((data) => data.category === "Free");
         console.log(data);
--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -21,7 +21,7 @@ function Signup() {
       password: data.password,
     };
     await axios
-      .post("http://localhost:5173/user/signup", userInfo)
+      .post("http://localhost:4001/user/signup", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
