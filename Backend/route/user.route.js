49690e9ddaefe20b5a5710b3c59da0bf6933d2fb
Krishna kant sharma <krishnactive@gmail.com>
2025-05-03 07:11:54 +0530

    deploy check

--- Backend/route/user.route.js
+++ Backend/route/user.route.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { signup, login } from "../controller/user.controller.js";
+import { signup, login, updateProfile } from "../controller/user.controller.js";
 import requireSignIn from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 
-// router.put("/profile", requireSignIn, updateProfile);
+router.put("/profile", requireSignIn, updateProfile);
 
 export default router;
